75313016a3d0c42cb18d3bb6d523340c3833b7fa
takayasu <a.takayasu@gmail.com>
2017-01-17 16:23:46 +0900

    追加

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import { createStore, applyMiddleware } from 'redux';
 import App from './components/app';
 import reducers from './reducers';
 
+import {Router,Route} from 'react-router';
+
+import {routes} from './route';
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router>
+      <Route path="/" component={App} />
+    </Router>
   </Provider>
   , document.querySelector('.container'));
-  
